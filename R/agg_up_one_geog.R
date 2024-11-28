6ac9253cfb2e6caefec54e9ba5a6a7dae39b949c
ML <7896923+lapsel@users.noreply.github.com>
2024-11-28 12:29:59 +0000

    documented and tested version of new agg_up_on_geog function

--- R/agg_up_one_geog.R
+++ R/agg_up_one_geog.R
@@ -1,25 +1,14 @@
-#' Add a column of GSS names
+#' Aggregate numeric data from one geogrpahy level to the next geography level up
 #' 
-#' Adds a column of GSS names to match a column of GSS codes.
-#'
-#' Uses the names that were operational for those GSS codes at the given date/year.
-#' 
-#' Throws an error if there are any GSS codes which were not operational at the 
-#' given date/year.
-#' 
-#' User must define the name of the column containing GSS codes, give a name
-#' for the new column to contain GSS names, and specify the date or year for the codes and names.
-#' 
-#' note that 2009 codes run up to 2012 but there was one name change in 2010
-#' and 2013 codes run up to 2019 but there was a name change in 2018.
-#' 
-#'
 #' @param df_in A data frame containing gss_codes and data.
 #' @param col_code A string. The name of the column which contains gss codes (defaults to
 #'   \code{gss_code}).
-#' @param col_name A string. The name to give the new column containing gss names. 
-#' Defaults to \code{gss_name}. If the column already exists in \code{df_in} it will be
-#' overwritten.
+#' @param col_name A string. The name of a column containing gss names. If there
+#' is no name column then this should be set to NA. Defaults to NA 
+#' @param col_data A string or character vector. The column(s) that contain the
+#' data to be aggregated. Defaults to 'value'
+#' @param fun Character. Function to be applied in aggregating data. Either 'sum'
+#'   or 'mean'. Default \code{'sum'}.
 #' @param gss_date A date object specifying the point in time that the gss codes 
 #' were/are operational. Only one of gss_date or gss_year can be defined. Defaults 
 #' to \code{NA})
@@ -28,12 +17,13 @@
 #' Only one of gss_date or gss_year can be defined. Defaults to \code{NA}) 
 #' 
 #'
-#' @return The input dataframe with a column either added or overwritten which contains
-#' the GSS names. The first two columns will be the gss codes and gss names respectively
+#' @return a dataframe with gss codes and values for one level of geography up
 #' 
 #' @import dplyr
 #' @importFrom lubridate is.Date
 #' @importFrom assertthat assert_that
+#' @import data.table
+#' @importFrom dtplyr lazy_dt
 #' 
 #' @export
 
@@ -41,33 +31,117 @@
 
 agg_up_one_geog <- function(df_in, 
                             col_code = "gss_code", 
-                            col_name = NA) {
+                            col_name = NA,
+                            col_data = "value",
+                            fun = "sum",
+                            gss_date = NA,
+                            gss_year = NA) {
+  
+  .validate_agg_up_one_geog(df_in, col_code, col_name, col_data, fun, gss_date, gss_year)
+  
+  # the function preserves the order of the columns
+  col_order <- names(df_in)
   
   df_in <- df_in %>%
     rename("gss_code" = !!col_code)
   
-  # maybe I want to make sure this function doesn't add a name column as a side effect
-  # if the name column is there then it will also need a date as will need to use add_gss_names after the aggregation
+  if (!is.na(gss_year)) {
+    gss_date = as.Date(paste0(gss_year,"-12-31"))
+  }
   
-  if (!!col_name %in% names(df_in)) {
+  # If there is a name column it is removed and the new names added at the end
+  if (!is.na(col_name)) {
     df_in <- df_in %>% select(-!!col_name)
   }
   
+  check_gss_codes(df_in, col_code = "gss_code", gss_date = gss_date, expect_complete = FALSE, geogs = NA)
+  
+  ####### Get the parent codes ########
+  
   parent_codes <- filter(all_codes_dates, gss_code %in% df_in$gss_code) %>%
+    filter(start_date <= gss_date & (end_date >= gss_date | is.na(end_date))) %>% # only get rows which were live at the given date
     select(gss_code, parent_cd) %>%
     unique()
   
+  # If Wales is in the data as a region, then add Wales as the parent code
+  # Assume Wales is a region if there are also E12 codes in the data.
+  if (any(grepl("^W92", parent_codes$gss_code)) & 
+      any(grepl("^E12", parent_codes$gss_code)) &
+      !any(grepl("^W06", parent_codes$gss_code))){
+    
+    parent_codes <- parent_codes %>%
+      mutate(parent_cd = ifelse(gss_code == "W92000004", "W92000004", parent_cd))
+    
+  }
   
-  # hmmm I'm going to need to specify the year in case a gss_code ever gets moved from one parent code to another
+  # TODO add error if there are any codes which don't have parent codes.
   
-  # must remove col_name if it exists, and then add the new col_name
+  df <- df_in %>% left_join(parent_codes, by = "gss_code")
   
-  .validate_agg_up_one_geog(df_in,col_code, col_name)
+  # Some LAD codes have E10 codes (counties) as parents but the rest have regions.
+  # For any codes with E10 as a parent, these will need to be parented again to get from county to region to match the rest of the LADs. 
+  df_default <- df %>% filter(!grepl("^E10", parent_cd))
+  df_e10 <- df %>% filter(grepl("^E10", parent_cd))
   
-
+  if (nrow(df_e10) != 0) {
+    df_e10 <- df_e10 %>%
+      select(-gss_code) %>%
+      rename(gss_code = parent_cd)
+    
+    check_gss_codes(df_e10, col_code = "gss_code", gss_date = gss_date, expect_complete = FALSE, geogs = NA)
+    
+    parent_codes <- filter(all_codes_dates, gss_code %in% df_e10$gss_code) %>%
+      filter(start_date <= gss_date & (end_date >= gss_date | is.na(end_date))) %>%
+      select(gss_code, parent_cd) %>%
+      unique()
+    
+    df_e10 <- df_e10 %>% left_join(parent_codes, by = "gss_code") %>%
+      select(names(df_default))
+    
+  }
+  
+  df <- bind_rows(df_default, df_e10) %>%
+    select(-gss_code) %>%
+    rename(gss_code = parent_cd)
+  
+  ####### Do the aggregation ########
+  
+  col_aggregation <- setdiff(names(df), col_data)
+  
+  if(fun == "sum"){
+    df <- df %>%
+      lazy_dt() %>%
+      group_by(across(!!col_aggregation)) %>%
+      summarise_all(.funs = sum) %>%
+      as.data.frame()
+  }
+  
+  if(fun == "mean"){
+    df <- df %>%
+      lazy_dt() %>%
+      group_by(across(!!col_aggregation)) %>%
+      summarise_all(.funs = mean) %>%
+      as.data.frame()
+  }
+  
+  
+  ####### Make sure df matches df_in ########
+  
+  if (!is.na(col_name)) {
+    df <- df %>% add_gss_names(col_code = col_code, col_name = col_name, gss_date = gss_date)
+  }
+  
+  
+  df <- as.data.frame(df) %>%
+    rename(!!col_code := "gss_code") %>%
+    select(all_of(col_order))
+  
+  return(df)
+  
+  
 }
 
-.validate_agg_up_one_geog <-function(df_in, col_code, col_name) {
+.validate_agg_up_one_geog <-function(df_in, col_code, col_name, col_data, fun, gss_date, gss_year) {
   
   # validate input variable data types
   assertthat::assert_that(is.data.frame(df_in),
@@ -76,8 +150,11 @@ agg_up_one_geog <- function(df_in,
   assertthat::assert_that(is.character(col_code),
                           msg = "in agg_up_one_geog, col_code must be of type character")
   
-  assertthat::assert_that(is.character(col_name),
+  assertthat::assert_that(is.na(col_name) | is.character(col_name),
                           msg = "in agg_up_one_geog, col_name must be of type character")
+  
+  assertthat::assert_that(is.character(col_data),
+                          msg = "in agg_up_one_geog, col_data must be of type character")
   
   assertthat::assert_that(is.na(gss_date) | is.Date(gss_date),
                           msg = "in agg_up_one_geog gss_date must be a date object")
@@ -89,6 +166,20 @@ agg_up_one_geog <- function(df_in,
   assertthat::assert_that(col_code %in% names(df_in),
                           msg = paste0("in agg_up_one_geog, specified col_code `", col_code,
                                        "` not in input dataframe"))
+  
+  assertthat::assert_that(is.na(col_name) | col_name %in% names(df_in),
+                          msg = paste0("in agg_up_one_geog, specified col_name `", col_name,
+                                       "` not in input dataframe"))
+  
+  for(i in length(col_data)){
+    
+    assertthat::assert_that(col_data[i] %in% names(df_in),
+                            msg = paste0("in agg_up_one_geog, specified col_data'", col_data[i],
+                                         "' not in input dataframe"))
+  }
+  
+  assertthat::assert_that(fun %in% c("sum","mean"),
+                          msg = "in agg_up_one_geog, fun must be sum or mean")
   
   assertthat::assert_that(is.na(gss_date) | is.na(gss_year),
                           msg = "in agg_up_one_geog only one of gss_date and gss_year can be specified")
--- tests/testthat/test-agg_up_one_geog.R
+++ tests/testthat/test-agg_up_one_geog.R
@@ -1,17 +1,94 @@
-library(dplyr)
 library(tidyr)
 
-load("R/sysdata.rda")
+# data for checking that the country and sex totals stay the same after aggregations
+set.seed(88)
 
-df_in_orig <- test_codes_names$y2023 %>% filter(entity %in% c("W06", "E06", "E07", "E08", "E09")) %>% select(-entity) %>%
+la_2023 <- test_codes_names$y2023 %>% filter(entity %in% c("W06", "E06", "E07", "E08", "E09")) %>% select(-entity) %>%
   group_by(gss_code, gss_name) %>%
   expand(sex = c("female", "male")) %>%
-  as.data.frame() %>% 
+  as.data.frame() %>%
   mutate(count = sample.int(100, n(), replace = TRUE))
+la_2023_totals <- la_2023 %>%
+  mutate(country = substr(gss_code, 1,1)) %>%
+  group_by(country, sex) %>%
+  summarise(count = sum(count)) %>%
+  as.data.frame() %>%
+  arrange(country, sex)
 
-head(df_in_orig)
+region_2023 <- la_2023 %>% agg_up_one_geog(col_name = "gss_name", col_data = "count", gss_year = 2023) 
+region_2023_totals <- region_2023 %>%
+  mutate(country = substr(gss_code, 1,1)) %>%
+  group_by(country, sex) %>%
+  summarise(count = sum(count)) %>%
+  as.data.frame() %>%
+  arrange(country, sex)
 
+country_2023 <- region_2023 %>% agg_up_one_geog(col_name = "gss_name", col_data = "count", gss_year = 2023) 
+country_2023_totals <- country_2023 %>%
+  mutate(country = substr(gss_code, 1,1)) %>%
+  group_by(country, sex) %>%
+  summarise(count = sum(count)) %>%
+  as.data.frame() %>%
+  arrange(country, sex)
 
-df_in <- df_in_orig
-col_code = "gss_code"
-col_name = "gss_name"
+# Select a few LAs to create a small dummy data set to check that the aggregations happen as expected when 
+# compared to manually aggregated data
+
+lads_gss <- c("E06000001", "E06000002", # region E12000001
+  "E07000008", "E07000009", "E07000066", "E07000067", # counties E10000003 & E10000012, region E12000006
+  "E08000001", "E08000002", # region E12000002
+  "E09000001", "E09000002", # region E12000007
+  "W06000001", "W06000002") # (region) W92000004
+
+lads_region <- c(rep("E12000001", 2),
+                 rep("E12000006", 4),
+                 rep("E12000002", 2),
+                 rep("E12000007", 2),
+                 rep("W92000004", 2))
+
+lads_counts <- c(1,2, 3,4,
+                 5,6,7,8, 9,10,11,12,
+                 13,14, 15,16,
+                 17,18, 19,20,
+                 21,22, 23,24)
+
+lads <- data.frame(gss_code = lads_gss, region = lads_region) %>%
+  add_gss_names(gss_year = 2023) %>%
+  group_by(gss_code, gss_name, region) %>%
+  expand(sex = c("female", "male")) %>%
+  as.data.frame()
+lads$count <- lads_counts
+
+regions <- lads %>%
+  group_by(region, sex) %>%
+  summarise(count = sum(count)) %>%
+  rename(gss_code = region) %>%
+  add_gss_names(gss_year = 2023) %>%
+  as.data.frame()
+
+country <- regions %>%
+  mutate(country = ifelse(grepl("^E", gss_code), "E92000001", "W92000004")) %>%
+  group_by(country, sex) %>%
+  summarise(count = sum(count)) %>%
+  rename(gss_code = country) %>%
+  as.data.frame() %>%
+  add_gss_names(gss_year = 2023)
+
+expect_lad <- regions %>% arrange(gss_code, sex)
+output_lad <- lads %>%
+  select(-region) %>%
+  agg_up_one_geog(col_name = "gss_name", col_data = "count", gss_year = 2023) %>%
+  arrange(gss_code, sex)
+
+expect_region <- country %>% arrange(gss_code, sex)
+output_region <- regions %>%
+  agg_up_one_geog(col_name = "gss_name", col_data = "count", gss_year = 2023) %>%
+  arrange(gss_code, sex)
+
+test_that("agg_up_one_geog produces the expected outputs", {
+  expect_equal(output_lad, expect_lad)
+  expect_equal(output_region, expect_region)
+  expect_equal(region_2023_totals, la_2023_totals)
+  expect_equal(country_2023_totals, la_2023_totals)
+  
+})
